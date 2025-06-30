ad3fe7f2b4070a3f49a3ade6b8ad13f13c1cc3be
hira4720 <hira.noor@jiotp.com>
2025-06-30 17:02:43 +0500

    update tags

--- src/logs_data/logs_data.service.ts
+++ src/logs_data/logs_data.service.ts
@@ -19,7 +19,7 @@ export class LogsDataService {
     reactive_power: ["ReactivePower_Total"],
     reactive_energy: ["ActiveEnergy_Total"],
     apparent_power: ["ApparentPower_Total"],
-    Harmonics: ["Harmonics_V1","Harmonics_V2","Harmonics_V3"],
+   Harmonics: ["Harmonics_V1_THD","Harmonics_V2_THD","Harmonics_V3_THD"],
     active_energy: ["Del_ActiveEnergy"],
   };
 
