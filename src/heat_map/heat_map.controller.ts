bf1148ba7cdd81f0f6b03e3e7735756c9fe365bf
hira4720 <hira.noor@jiotp.com>
2025-07-18 10:28:21 +0500

    update the heat map code

--- src/heat_map/heat_map.controller.ts
+++ src/heat_map/heat_map.controller.ts
@@ -1,31 +1,15 @@
-// import { Controller, Get, Query } from '@nestjs/common';
-// import { HeatMapService } from './heat_map.service';
-// import { HeatMapDto } from './dto/heat_map.dto';
-
-// @Controller('heat-map')
-// export class HeatMapController {
-//   constructor(private readonly heatMapService: HeatMapService) {}
-
-//   @Get('hourly')
-//   async getHourlyConsumption(@Query() dto: HeatMapDto) {
-//     return this.heatMapService.getHourlyConsumption(dto.start_date, dto.end_date);
-//   }
-// }
-
 import { Controller, Get, Query } from '@nestjs/common';
-import {  HeatMapService } from './heat_map.service';
+import { HeatMapService } from './heat_map.service';
 
 @Controller()
 export class HeatMapController {
-  constructor(private readonly  HeatMapService:  HeatMapService) {}
+  constructor(private readonly heatMapService: HeatMapService) {}
 
   @Get('heat-map')
-  async getConVsPro(
+  async getHeatMapData(
     @Query('start_date') startDate: string,
-    @Query('end_date') endDate: string,
-    @Query('label') label: string
+    @Query('end_date') endDate: string
   ) {
-    return this. HeatMapService.getPowerData(startDate, endDate, label);
+    return this.heatMapService.getPowerAverages(startDate, endDate);
   }
 }
-
