64d0a506950a2ef97485b2952312d0c42e040658
hira4720 <hira.noor@jiotp.com>
2025-07-25 16:21:25 +0500

    pushed the code

--- src/energy/energy.service.ts
+++ src/energy/energy.service.ts
@@ -19,13 +19,13 @@ export class EnergyService {
         "U15_GW02", "U16_GW02", "U17_GW02", "U18_GW02", "U19_GW02", "U20_GW02", "U21_GW02", "U22_GW02", "U23_GW02", "U1_GW03", "U2_GW03", "U3_GW03",
         "U4_GW03", "U5_GW03", "U6_GW03", "U18_GW03", "U19_GW03", "U20_GW03", "U21_GW03", "U22_GW03", "U23_GW03"];
     const suffixes: string[] = ['Del_ActiveEnergy', 'ActivePower_Total'];
-    const LTGenerationKeys = ['U19_PLC_Del_ActiveEnergy', 'U21_PLC_Del_ActiveEnergy'];
+    const LTGenerationKeys = ['U19_PLC_Del_ActiveEnergy', 'U11_GW01_Del_ActiveEnergy'];
     const SolarGenerationKeys = ['U6_GW02_Del_ActiveEnergy', 'U17_GW03_Del_ActiveEnergy'];
     const HTGenerationKeys = ['U20_GW03_Del_ActiveEnergy','U21_GW03_Del_ActiveEnergy','U23_GW01_Del_ActiveEnergy', 'U7_GW01_Del_ActiveEnergy',
       ];
 
-    const WapdaImportKeys = ['U22_GW01_Del_ActiveEnergy'];
-    const WapdaExportKeys = ['U22_GW01_ActiveEnergy_Exp_kWh'];
+    const WapdaImportKeys = ['U13_GW02_ActiveEnergy_Imp_kWh', 'U16_GW03_ActiveEnergy_Imp_kWh'];
+    const WapdaExportKeys = ['U13_GW02_ActiveEnergy_Exp_kWh', 'U16_GW03_ActiveEnergy_Exp_kWh'];
     const Trafo1IncomingKeys = ['U21_PLC_Del_ActiveEnergy'];
     // const Trafo1activepowertotalKeys = ['U21_PLC_ActivePower_Total'];
     const Trafo2IncomingKeys = ['U13_GW01_Del_ActiveEnergy'];
